174753f66849197b2b1601aa335258abbe78e723
Mafesita23 <116750999+Mafesita23@users.noreply.github.com>
2023-01-10 15:00:26 -0500

    Imagenes bien
    Imagenes bien

--- src/data/works.js
+++ src/data/works.js
@@ -2,28 +2,28 @@ export default [
 	{
 		title: 'Calculadora',
 		imgUrl:
-			'./images/Calculadoragif.gif',
+			'./src/images/Calculadoragif.gif',
 		tech: ['JavaScript', 'CSS', 'HTML','GIT','FIGMA'],
 		workUrl: 'https://github.com/Mafesita23/Calculadora-Mafe',
 	},
 	{
 		title: "Wheel Of Doom",
 		imgUrl:
-			'./images/WheelOfDoom.gif',
+			'./src/images/WheelOfDoom.gif',
 		tech: ['JavaScript','HTML', 'CSS', 'GIT','FIGMA'],
 		workUrl: 'https://github.com/jefdavgar/TINGUA',
 	},
 	{
 		title: "Humedinformate",
 		imgUrl:
-		'./images/Humedinformate.gif',
+		'./src/images/Humedinformate.gif',
 		tech: ['HTML', 'CSS', 'FIGMA'],
 		workUrl: 'https://github.com/Mafesita23/humedales',
 	},
 	{
 		title: 'Consumo de API',
 		imgUrl:
-		'./images/APIrickandmorty.gif',
+		'./src/images/APIrickandmorty.gif',
 		tech: ['React JS', 'CSS'],
 		workUrl: 'https://github.com/Mafesita23/RickandMortyAPI',
 	},
